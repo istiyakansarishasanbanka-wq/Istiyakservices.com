4ec82cda203af1b02695f555c88a061f8d1495fd
istiyakansarishasanbanka-wq <istiyakansarishasanbanka@gmail.com>
2025-10-15 11:14:37 +0530

    Create about.jsx

---
+++ about.jsx
@@ -0,0 +1,11 @@
+function About() {
+  return (
+    <div className="container">
+      <h2>About Us</h2>
+      <p>
+        Welcome to Istiyak Service Point. Our mission is to provide all government and online services in one place for easy access and convenience.
+      </p>
+    </div>
+  );
+}
+export default About;
