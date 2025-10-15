4846f63f93a8bda9c614093237755a852a1eff86
istiyakansarishasanbanka-wq <istiyakansarishasanbanka@gmail.com>
2025-10-15 11:17:03 +0530

    Create signup.jsx

---
+++ signup.jsx
@@ -0,0 +1,14 @@
+function Signup() {
+  return (
+    <div className="container">
+      <h2>Sign Up</h2>
+      <form>
+        <input type="text" placeholder="Name" required />
+        <input type="email" placeholder="Email" required />
+        <input type="password" placeholder="Password" required />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+}
+export default Signup;
