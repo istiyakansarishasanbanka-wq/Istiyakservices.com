63d573f8f9ed3e06d6a399de142fae96dba1813b
istiyakansarishasanbanka-wq <istiyakansarishasanbanka@gmail.com>
2025-10-15 11:16:00 +0530

    Create Contact.jsx

---
+++ Contact.jsx
@@ -0,0 +1,15 @@
+function Contact() {
+  return (
+    <div className="container">
+      <h2>Contact Us</h2>
+      <p>Email: istiyakansari3110@gmail.com</p>
+      <p>Location: India</p>
+      <div>
+        <a href="https://www.facebook.com/share/16Uxe2HUTz/" target="_blank">Facebook</a> |
+        <a href="https://www.instagram.com/future_with_skills6" target="_blank">Instagram</a> |
+        <a href="https://youtube.com/@istiyak7techz" target="_blank">YouTube</a>
+      </div>
+    </div>
+  );
+}
+export default Contact;
