beca5722b7c0ec0da495020471f051606e619f64
istiyakansarishasanbanka-wq <istiyakansarishasanbanka@gmail.com>
2025-10-15 11:16:31 +0530

    Create login.jsx

---
+++ login.jsx
@@ -0,0 +1,13 @@
+function Login() {
+  return (
+    <div className="container">
+      <h2>Login</h2>
+      <form>
+        <input type="email" placeholder="Email" required />
+        <input type="password" placeholder="Password" required />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+export default Login;
